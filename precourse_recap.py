48b87c0ec1dc454bca5e4188572c9aff48b30327
little-radish <roisin@littleradish.dev>
2022-07-05 08:16:01 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,18 @@
+hours = 10
+building = "clockwise"
+location = "glasgow"
+
+userresponse = input("What would you like to check?")
+
+if userresponse = "building":
+    print("The building name was" + building)
+
+elif userresponse = "hours":
+    print("You were out for " + hours + "hours")
+    coffeecheck = input("Wait, did you stop for coffee?")
+    if coffeecheck = "Yes":
+        hours + 1
+        print("Oh, you were out for " + hours + "hours in that case")
+
+elif userresponse = "location"
+    print("You were in " + location)
